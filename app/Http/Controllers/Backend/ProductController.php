7cb34f18fccc382f75461620892acacc30130c85
DanDai <portilla.dan@gmail.com>
2022-01-17 18:13:28 -0800

    Agregar productos

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -9,6 +9,9 @@ use App\Models\SubCategory;
 use App\Models\SubSubCategory;
 use App\Models\Brand;
 use App\Models\Product;
+use App\Models\MultiImg;
+use Carbon\Carbon;
+use Image;
 
 class ProductController extends Controller
 {
@@ -16,5 +19,71 @@ class ProductController extends Controller
         $categories = Category::latest()->get();
         $brands = Brand::latest()->get();
         return view('backend.product.product_add',compact('categories','brands'));
+    }
+
+
+    public function StoreProduct(Request $request){
+
+        $image = $request->file('product_thumbnail');
+        $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+        // mkdir('upload/products/',0755,TRUE);
+        // mkdir('upload/products/thumnails/',0755,TRUE);
+        Image::make($image)->resize(917,1000)->save('upload/products/thumnails/'.$name_gen);
+        $save_url = 'upload/products/thumnails/'.$name_gen;
+
+        $product_id = Product::insertGetId([
+            'brand_id' => $request->brand_id,
+            'category_id' => $request->category_id,
+            'subcategory_id' => $request->subcategory_id,
+            'subsubcategory_id' => $request->subsubcategory_id,
+            'product_name_eng' => $request->product_name_eng,
+            'product_name_esp' => $request->product_name_esp,
+            'product_slug_eng' => strtolower(str_replace(' ','-',$request->product_name_eng)),
+            'product_slug_esp' => strtolower(str_replace(' ','-',$request->product_name_esp)),
+            'product_code' => $request->product_code,
+            'product_qty' => $request->product_qty,
+            'product_tags_eng' => $request->product_tags_eng,
+            'product_tags_esp' => $request->product_tags_esp,
+            'product_size_eng' => $request->product_size_eng,
+            'product_size_esp' => $request->product_size_esp,
+            'product_color_eng' => $request->product_color_eng,
+            'product_color_esp' => $request->product_color_esp,
+            'selling_price' => $request->selling_price,
+            'discount_price' => $request->discount_price,
+            'short_description_eng' => $request->short_description_eng,
+            'short_description_esp' => $request->short_description_esp,
+            'long_description_eng' => $request->long_description_eng,
+            'long_description_esp' => $request->long_description_esp,
+            'hot_deals' => $request->hot_deals,
+            'featured' => $request->featured,
+            'special_offer' => $request->special_offer,
+            'special_deals' => $request->special_deals,
+            'product_thumbnail' => $save_url,
+            'status' => 1,
+            'created_at' => Carbon::now(),
+        ]);
+
+
+        $images = $request->file('multi_img');
+        // mkdir('upload/products/multi-image/',0755,TRUE);
+        foreach ($images as $img) {
+            $make_name = hexdec(uniqid()).'.'.$img->getClientOriginalExtension();
+            Image::make($img)->resize(917,1000)->save('upload/products/multi-image/'.$make_name);
+            $uploadPath = 'upload/products/multi-image/'.$make_name;
+
+            MultiImg::insert([
+                'product_id' => $product_id,
+                'photo_name' => $uploadPath,
+                'created_at' => Carbon::now(),
+            ]);
+        }
+
+
+        $notification = array(
+            'message' => 'Product Inserted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
     }
 }
--- resources/views/backend/product/product_add.blade.php
+++ resources/views/backend/product/product_add.blade.php
@@ -17,7 +17,8 @@
     <div class="box-body">
         <div class="row">
         <div class="col">
-            <form novalidate>
+            <form method="post" action="{{ route('product-store') }}" enctype="multipart/form-data">
+                @csrf
                 <div class="row">
                 <div class="col-12">
 
@@ -27,7 +28,7 @@
                             <div class="form-group">
                                 <h5>Brand Select <span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <select name="brand_id" class="form-control">
+                                    <select name="brand_id" class="form-control" required="">
                                         <option value="" selected="" disabled="">Select Brand</option>
                                         @foreach($brands as $brand)
                                             <option value="{{ $brand->id }}">{{ $brand->brand_name_en }}</option>
@@ -44,7 +45,7 @@
                             <div class="form-group">
                                 <h5>Category Select <span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <select name="category_id" class="form-control">
+                                    <select name="category_id" class="form-control" required="">
                                         <option value="" selected="" disabled="">Select Category</option>
                                         @foreach($categories as $category)
                                             <option value="{{ $category->id }}">{{ $category->category_name_eng }}</option>
@@ -61,7 +62,7 @@
                             <div class="form-group">
                                 <h5>SubCategory Select <span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <select name="subcategory_id" class="form-control">
+                                    <select name="subcategory_id" class="form-control" required="">
                                         <option value="" selected="" disabled="">Select SubCategory</option>
                                     </select>
                                     @error('subcategory_id')
@@ -81,7 +82,7 @@
                             <div class="form-group">
                                 <h5>Sub-SubCategory Select <span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <select name="subsubcategory_id" class="form-control">
+                                    <select name="subsubcategory_id" class="form-control" required="">
                                         <option value="" selected="" disabled="">Select Sub-SubCategory</option>
                                     </select>
                                     @error('subsubcategory_id')
@@ -95,7 +96,7 @@
                             <div class="form-group">
                                 <h5>Product Name Eng<span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <input type="text" name="product_name_eng" class="form-control">
+                                    <input type="text" name="product_name_eng" class="form-control" required="">
                                     @error('product_name_eng')
                                         <span class="text-danger">{{ $message }}</span>
                                     @enderror
@@ -107,7 +108,7 @@
                             <div class="form-group">
                                 <h5>Product Name Esp<span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <input type="text" name="product_name_esp" class="form-control">
+                                    <input type="text" name="product_name_esp" class="form-control" required="">
                                     @error('product_name_esp')
                                         <span class="text-danger">{{ $message }}</span>
                                     @enderror
@@ -126,7 +127,7 @@
                             <div class="form-group">
                                 <h5>Product Code<span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <input type="text" name="product_code" class="form-control">
+                                    <input type="text" name="product_code" class="form-control" required="">
                                     @error('product_code')
                                         <span class="text-danger">{{ $message }}</span>
                                     @enderror
@@ -138,7 +139,7 @@
                             <div class="form-group">
                                 <h5>Product Quantity<span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <input type="text" name="product_qty" class="form-control">
+                                    <input type="text" name="product_qty" class="form-control" required="">
                                     @error('product_qty')
                                         <span class="text-danger">{{ $message }}</span>
                                     @enderror
@@ -150,7 +151,7 @@
                             <div class="form-group">
                                 <h5>Product Tags Eng<span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <input type="text" name="product_tags_eng" class="form-control" value="Lorem,Ipsum,Amet" data-role="tagsinput">
+                                    <input type="text" name="product_tags_eng" class="form-control" value="Lorem,Ipsum,Amet" data-role="tagsinput" required="">
                                     @error('product_tags_eng')
                                         <span class="text-danger">{{ $message }}</span>
                                     @enderror
@@ -169,7 +170,7 @@
                             <div class="form-group">
                                 <h5>Product Tags Esp<span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <input type="text" name="product_tags_esp" class="form-control" value="Lorem,Ipsum,Amet" data-role="tagsinput">
+                                    <input type="text" name="product_tags_esp" class="form-control" value="Lorem,Ipsum,Amet" data-role="tagsinput" required="">
                                     @error('product_tags_esp')
                                         <span class="text-danger">{{ $message }}</span>
                                     @enderror
@@ -181,7 +182,7 @@
                             <div class="form-group">
                                 <h5>Product Size Eng<span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <input type="text" name="product_size_eng" class="form-control" value="Small,Medium,Large" data-role="tagsinput">
+                                    <input type="text" name="product_size_eng" class="form-control" value="Small,Medium,Large" data-role="tagsinput" required="">
                                     @error('product_size_eng')
                                         <span class="text-danger">{{ $message }}</span>
                                     @enderror
@@ -193,7 +194,7 @@
                             <div class="form-group">
                                 <h5>Product Size Esp<span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <input type="text" name="product_size_esp" class="form-control" value="Chico,Mediano,Grande" data-role="tagsinput">
+                                    <input type="text" name="product_size_esp" class="form-control" value="Chico,Mediano,Grande" data-role="tagsinput" required="">
                                     @error('product_size_esp')
                                         <span class="text-danger">{{ $message }}</span>
                                     @enderror
@@ -212,7 +213,7 @@
                             <div class="form-group">
                                 <h5>Product Color Esp<span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <input type="text" name="product_color_eng" class="form-control" value="Red,Black,White" data-role="tagsinput">
+                                    <input type="text" name="product_color_eng" class="form-control" value="Red,Black,White" data-role="tagsinput" required="">
                                     @error('product_color_eng')
                                         <span class="text-danger">{{ $message }}</span>
                                     @enderror
@@ -224,7 +225,7 @@
                             <div class="form-group">
                                 <h5>Product Color Esp<span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <input type="text" name="product_color_esp" class="form-control" value="Rojo,Negro,Blanco" data-role="tagsinput">
+                                    <input type="text" name="product_color_esp" class="form-control" value="Rojo,Negro,Blanco" data-role="tagsinput" required="">
                                     @error('product_color_esp')
                                         <span class="text-danger">{{ $message }}</span>
                                     @enderror
@@ -236,7 +237,7 @@
                             <div class="form-group">
                                 <h5>Product Selling Price<span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <input type="text" name="selling_price" class="form-control">
+                                    <input type="text" name="selling_price" class="form-control" required="">
                                     @error('selling_price')
                                         <span class="text-danger">{{ $message }}</span>
                                     @enderror
@@ -255,7 +256,7 @@
                             <div class="form-group">
                                 <h5>Product Discount Price<span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <input type="text" name="discount_price" class="form-control">
+                                    <input type="text" name="discount_price" class="form-control" required="">
                                     @error('discount_price')
                                         <span class="text-danger">{{ $message }}</span>
                                     @enderror
@@ -267,10 +268,11 @@
                             <div class="form-group">
                                 <h5>Main Product Thumbnail<span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <input type="file" name="product_thumbnail" class="form-control">
+                                    <input type="file" name="product_thumbnail" class="form-control" onChange="mainThumbUrl(this)" required="">
                                     @error('product_thumbnail')
                                         <span class="text-danger">{{ $message }}</span>
                                     @enderror
+                                    <img src="" alt="" id="mainThumb">
                                 </div>
                             </div>
                         </div>
@@ -279,10 +281,13 @@
                             <div class="form-group">
                                 <h5>Multi-Image<span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <input type="file" name="multi_img[]" class="form-control">
+                                    <input type="file" name="multi_img[]" class="form-control" multiple="" id="multiImg" required="">
                                     @error('multi_img')
                                         <span class="text-danger">{{ $message }}</span>
                                     @enderror
+                                    <div class="row" id="preview_img">
+
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -299,7 +304,7 @@
                             <div class="form-group">
                                 <h5>Short Description Eng<span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <textarea name="short_description_eng" id="textarea" class="form-control" required placeholder="Textarea text"></textarea>
+                                    <textarea name="short_description_eng" id="textarea" class="form-control" required placeholder="Textarea text" required=""></textarea>
                                 </div>
                             </div>
                         </div>
@@ -308,7 +313,7 @@
                             <div class="form-group">
                                 <h5>Short Description Esp<span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <textarea name="short_description_esp" id="textarea" class="form-control" required placeholder="Textarea text"></textarea>
+                                    <textarea name="short_description_esp" id="textarea" class="form-control" required placeholder="Textarea text" required=""></textarea>
                                 </div>
                             </div>
                         </div>
@@ -326,7 +331,7 @@
                             <div class="form-group">
                                 <h5>Long Description Eng<span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <textarea id="editor1" name="long_description_eng" rows="10" cols="80">
+                                    <textarea id="editor1" name="long_description_eng" rows="10" cols="80" required="">
                                         Long Description English.
                                     </textarea>
                                 </div>
@@ -337,7 +342,7 @@
                             <div class="form-group">
                                 <h5>Long Description Esp<span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <textarea id="editor2" name="long_description_esp" rows="10" cols="80">
+                                    <textarea id="editor2" name="long_description_esp" rows="10" cols="80" required="">
                                         Long Description Spanish.
                                     </textarea>
                                 </div>
@@ -444,6 +449,49 @@
             }
         });
     });
+</script>
+
+
+
+<script type="text/javascript">
+	function mainThumbUrl(input){
+		if (input.files && input.files[0]) {
+			var reader = new FileReader();
+			reader.onload = function(e){
+				$('#mainThumb').attr('src',e.target.result).width(80).height(80);
+			};
+			reader.readAsDataURL(input.files[0]);
+		}
+	}	
+</script>
+
+
+<script>
+  $(document).ready(function(){
+   $('#multiImg').on('change', function(){ //on file input change
+      if (window.File && window.FileReader && window.FileList && window.Blob) //check File API supported browser
+      {
+          var data = $(this)[0].files; //this file data
+           
+          $.each(data, function(index, file){ //loop though each file
+              if(/(\.|\/)(gif|jpe?g|png)$/i.test(file.type)){ //check supported file type
+                  var fRead = new FileReader(); //new filereader
+                  fRead.onload = (function(file){ //trigger function on successful read
+                  return function(e) {
+                      var img = $('<img/>').addClass('thumb').attr('src', e.target.result) .width(80)
+                  .height(80); //create image element 
+                      $('#preview_img').append(img); //append image to output element
+                  };
+                  })(file);
+                  fRead.readAsDataURL(file); //URL representing the file's data.
+              }
+          });
+           
+      }else{
+          alert("Your browser doesn't support File API!"); //if File API is absent
+      }
+   });
+  });
 </script>
 
 @endsection
